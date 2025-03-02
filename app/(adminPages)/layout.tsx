5240f28e0667567cbf411308eb693cf6b2aea2f8
Moe_Moaid <mohammadmoaid@gmail.com>
2025-03-02 23:51:19 +0500

    cleaned up some code

--- app/(adminPages)/layout.tsx
+++ app/(adminPages)/layout.tsx
@@ -1,19 +1,18 @@
-'use client';
+"use client";
 import React from "react";
 
 export default function AdminLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <div className="bg-[#E4E4E4]">
-      <p className="font-bold text-[20px] text-red-500">
-        this is admin layout
-      </p>
-      <div className="w-100 justify-center items-center ">
-        {children}
-      </div>
-    </div>
-  )
+    <html lang="en">
+      <body className="bg-[#E4E4E4]">
+        <div>
+          <div className="w-100 justify-center items-center ">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
 }
