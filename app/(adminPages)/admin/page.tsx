579d24cf0ab0c17e5650869e17adc2bede9115a8
Mohammad Moaid <79970218+moe-moaid@users.noreply.github.com>
2025-03-05 23:44:04 +0500

    main form structure is done (#4)
    * main form structure is done
    
    * added dynamic delete itmes from edit form and enhanced UI in general

--- app/(adminPages)/admin/page.tsx
+++ app/(adminPages)/admin/page.tsx
@@ -113,49 +113,6 @@ const [menuItems, setMenuItems] = useState<ListItem[]>([
           <RenderForms menueStatus={ menuItems } />
         </div>
       </div>
-    <div className="flex flex-col max-w-7xl justify-center items-center mx-auto">
-      <form className="flex flex-col space-y-5">
-        <input
-          onChange={(e) => setTitel(e.target.value)}
-          className="contactInput"
-          name="title"
-          type="text"
-          placeholder="title"
-          required
-        />
-        <input
-          onChange={(e) => setMessage(e.target.value)}
-          className="contactInput"
-          name="description"
-          type="text"
-          placeholder="about you"
-          required
-        />
-        <input
-          onChange={(e) => {
-            const selectedFile = (e.target.files as FileList)[0];
-            setFile(selectedFile);
-          }}
-          id="aboutImageInput"
-          className="contactInput"
-          name="aboutImage"
-          type="file"
-          required
-        />
-        <button
-          className="bg-blue-500 rounded-md p-2 font-semibold text-white/80"
-          onClick={hitApi}
-        >
-          Enter Value
-        </button>
-        <button
-          className="bg-blue-500 rounded-md p-2 mt-5 font-semibold text-white/80"
-          onClick={getApi}
-        >
-          get data
-        </button>
-      </form>
-    </div>
     </div>
   );
 }
