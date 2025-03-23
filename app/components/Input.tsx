99d310be627eff566b191f618c2af4454665f255
Moe_Moaid <mohammadmoaid@gmail.com>
2025-03-23 19:26:17 +0500

    keeping int moving

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -23,3 +23,32 @@ export default function Input<HTMLInputElement>({
     />
   );
 }
+
+/**
+ * import React, { InputHTMLAttributes, useState } from "react";
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  placeHolder?: string;
+  name?: string;
+  value?: string | number;
+  onChange?: (e: any) => void;
+}
+export default function Input<HTMLInputElement>({
+  placeHolder,
+  name,
+  value,
+  onChange,
+}: Props) {
+  const [input, setInput] = useState<string>("");
+  return (
+    <input
+      type="text"
+      name={name}
+      value={value}
+      onChange={onChange}
+      placeholder={placeHolder}
+      className="p-2 outline-none border-b border-[#2EC4CE] w-full placeholder:text-black text-black"
+    />
+  );
+}
+
+ */
