c844b07d6d001275de40a820a300bc26b3b17e60
Moe_Moaid <mohammadmoaid@gmail.com>
2025-05-02 23:57:20 +0500

    diabled functionality added

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,16 +2,18 @@ import React from "react";
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   style_type: "primary" | "secondary";
   fullWidth: boolean;
+  disabled?: boolean;
   text: string;
   onClick?: (e: any) => void;
 }
-export default function Button({ style_type, fullWidth, text, onClick }: Props) {
+export default function Button({ style_type, fullWidth, text, onClick, disabled }: Props) {
   return (
     <button
       className={`${fullWidth ? "w-full py-2" : "px-2 py-1"} ${
         style_type === "primary" ? "bg-[#2E8CFA]" : ""
       } font-medium text-[12px] text-white rounded-md`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
